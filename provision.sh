c5170b9b15fa7b86842ea3542a07ca5bc7ecd2b3
Ubuntu <ubuntu@ip-172-31-31-128.us-west-2.compute.internal>
2015-12-01 22:24:16 +0000

    cleanup

--- provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd neon
-. .venv/bin/activate
-cd ../meetup
-ipython notebook --ip 0.0.0.0 --no-browser
-
---
+++ utils/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# activate neon virtual env
+cd ~/neon
+. .venv/bin/activate
+cd ~/meetup
+
+# ensure notebooks are read only
+chmod u-w cifar_example.ipynb
+chmod u-w imdb_with_prepro.ipynb
+
+# launch notebook server
+ipython notebook --ip 0.0.0.0 --no-browser
+
