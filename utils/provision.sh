81a44db50cfaa26a20d5657ac7a7e104cabfd0f3
Will Constable <will@nervanasys.com>
2016-03-04 22:54:17 +0000

    updated provisioning script

--- utils/provision.sh
+++ utils/provision.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+export PATH="/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+
 # activate neon virtual env
-cd ~/neon
+cd /home/ubuntu/neon
 . .venv/bin/activate
-cd ~/meetup
+cd /home/ubuntu/meetup
 
 # ensure notebooks are read only
 chmod u-w cifar_example.ipynb
